75876eedcdb1af829905f47f012b30e911c3fd84
fullstackfreak <sureshkumar.oneteam@gmail.com>
2025-05-20 16:22:36 +0530

    jis

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const feedbackRoute = require("./routes/feedbackRoute");
 const app = express();
 
 const MONGO_URI = process.env.MONGO_URI;
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8080;
 app.use(
   cors({
     origin: "https://feedback-app-eight-iota.vercel.app",
