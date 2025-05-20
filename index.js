0a94e4dca7d5158ed66c15b0a24eb3295a93d0b5
fullstackfreak <sureshkumar.oneteam@gmail.com>
2025-05-20 16:39:16 +0530

    init-3

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 const port = process.env.PORT || 8080;
 
 app.get("/", (req, res) => {
-  res.send("Subscribe to Arpan Neupane's channel");
+  res.send("Working");
 });
 
 app.listen(port, () => {
